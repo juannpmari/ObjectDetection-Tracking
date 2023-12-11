1d393af1e3e0c44d4182040e663917fef7b41cbd
juan.mari <juan.mari@neoris.com>
2023-12-11 11:04:43 -0300

    defined inferencie function

--- inference.py
+++ inference.py
@@ -1,7 +1,13 @@
-
+from ultralytics import YOLO
 #TODO: use argparser to launch inference with certain hyperam and a certain model.
 
+def predict(image,output_path,model = 'yolov8.pt'):
+    '''Runs predictions over an image/set of images and saves them to a new image/video'''
+    model = YOLO(model)
+    pred_result = model(image)
+    #Save to image or video in output_path
 
+#Borrar:
 # if __name__=='__main__':
-    # video_path = 'data/train/MOT16-02' #En realidad sería el path a los videos con las predicciones hechas
-    # du.video_visualizer(video_path)    
+#     video_path = 'data/test/MOT16-02' #En realidad sería el path a los videos con las predicciones hechas
+#     du.video_visualizer(video_path)    
